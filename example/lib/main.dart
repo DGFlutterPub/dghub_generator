275e744787d6710c819a3f393050ed10b1f9e9f7
dghub <advancer@Advancers-MBP.local>
2024-05-03 20:17:31 +0630

    update

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:example/config/providers.dart';
+import 'package:example/system/scroll/scroll_behavior.dart';
 import 'system/theme/theme_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:dghub_generator/dghub_generator.dart';
@@ -6,6 +7,7 @@ import 'package:dghub_generator/dghub_generator.dart';
 import 'system/language/language_provider.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const DGHubApp());
 }
 
@@ -21,6 +23,10 @@ class DGHubApp extends StatelessWidget {
         return Consumer<LanguageProvider>(
             builder: (languageContext, languageProvider, languageChild) {
           return MaterialApp.router(
+            scrollBehavior: DGHubScrollBehavior.scrollBehavior(),
+            builder: (context, child) {
+              return DGHubScrollBehavior.scrollBuilderWidget(child: child);
+            },
             theme: ThemeData(
               colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
               useMaterial3: true,
