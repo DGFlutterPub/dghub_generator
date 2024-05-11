07d758565fc923217e52dd2ec9eab507e88aa458
dghub <advancer@Advancers-MBP.local>
2024-05-11 22:38:42 +0630

    update

--- example/lib/gen/auth/controllers/auth_controller.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-class AuthController extends Notifier {
-  @override
-  build() {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}
--- example/lib/system/theme/theme_controller.dart
+++ example/lib/system/theme/theme_controller.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:objectbox/internal.dart';
+import 'package:objectbox/objectbox.dart';
 
 class ThemeController extends Notifier<ThemeMode> {
   @override
