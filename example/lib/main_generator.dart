bf1a9f1c206f44cb8a83e4777caafde54dae4664
dghub-founder <dghub.dev@gmail.com>
2024-10-21 20:13:30 +0630

    updated

--- example/lib/main_generator.dart
+++ example/lib/main_generator.dart
@@ -9,5 +9,7 @@ import "package:dghub_generator/dghub_generator.dart";
         baseUrl: 'http://127.0.0.1',
         devUrl: 'http://127.0.0.1',
         production: true,
-        port: 6001))
+        port: 6001),
+    iconConfig:
+        DGIconConfig(android: true, androidPath: 'assets/images/icon.png'))
 class MainGenerator {}
--- lib/src/models/dg_app_config.g.dart
+++ lib/src/models/dg_app_config.g.dart
@@ -7,16 +7,19 @@ part of 'dg_app_config.dart';
 // **************************************************************************
 
 DGAppConfig _$DGAppConfigFromJson(Map<String, dynamic> json) => DGAppConfig(
-      enabledGenerator: json['enabledGenerator'] as bool? ?? true,
-      appName: json['appName'] as String? ?? 'Example (DG)',
-      packageName: json['packageName'] as String? ?? 'dev.dghub.example',
-      apiVersion: json['apiVersion'] as String? ?? 'v1',
-      baseUrl: json['baseUrl'] as String? ?? 'http://127.0.0.1',
-      devUrl: json['devUrl'] as String? ?? 'http://127.0.0.1',
-      port: (json['port'] as num?)?.toInt() ?? 6001,
-      socketVersion: json['socketVersion'] as String? ?? 'v1',
-      production: json['production'] as bool? ?? false,
-    );
+    enabledGenerator: json['enabledGenerator'] as bool? ?? true,
+    appName: json['appName'] as String? ?? 'Example (DG)',
+    packageName: json['packageName'] as String? ?? 'dev.dghub.example',
+    apiVersion: json['apiVersion'] as String? ?? 'v1',
+    baseUrl: json['baseUrl'] as String? ?? 'http://127.0.0.1',
+    devUrl: json['devUrl'] as String? ?? 'http://127.0.0.1',
+    port: (json['port'] as num?)?.toInt() ?? 6001,
+    socketVersion: json['socketVersion'] as String? ?? 'v1',
+    production: json['production'] as bool? ?? false,
+    secertKey: json['secertKey'] as String? ??
+        'a5HqSZZ3jYIuufrD3afk25exxS08YI3wBqpScJz9qz7ugG7YCQUcNGS29XRVLqHe',
+    publicKey: json['publicKey'] as String? ??
+        'e5HqSZZ3jYIuufrD3afk25exxS08YI3wBqpSMJz9qz7ugG7YCQUcNGS29XRVLqHa');
 
 Map<String, dynamic> _$DGAppConfigToJson(DGAppConfig instance) =>
     <String, dynamic>{
@@ -28,5 +31,7 @@ Map<String, dynamic> _$DGAppConfigToJson(DGAppConfig instance) =>
       'devUrl': instance.devUrl,
       'socketVersion': instance.socketVersion,
       'port': instance.port,
-      'production': instance.production
+      'production': instance.production,
+      'secertKey': instance.secertKey,
+      'publicKey': instance.publicKey
     };
